65051ef09f7209c4642600721d869d03d57d40b7
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2025-03-28 10:44:23 -0400

    Update Dockerfile to install dependencies and OBS Studio

--- pi-obs-container/entrypoint.sh
+++ pi-obs-container/entrypoint.sh
@@ -3,11 +3,23 @@
 # Detect connected video inputs
 /usr/local/bin/detect-devices.sh
 
-# If display available, launch full GUI OBS
+# Setup OBS Scene Collection
+mkdir -p /root/.config/obs-studio/basic/scenes/
+cp /config/basic/scenes/*.json /root/.config/obs-studio/basic/scenes/
+
+# Setup OBS Profiles
+mkdir -p /root/.config/obs-studio/basic/profiles/CdaprodOBS/
+cp /config/basic/profiles/CdaprodOBS/* /root/.config/obs-studio/basic/profiles/CdaprodOBS/
+
+# OBS-Websocket Config
+mkdir -p /root/.config/obs-studio/plugin_config/obs-websocket/
+cp /config/plugin_config/obs-websocket/config.json /root/.config/obs-studio/plugin_config/obs-websocket/
+
+# Launch OBS directly (GUI or CLI)
 if [ -n "$DISPLAY" ]; then
   echo "Starting OBS with GUI..."
-  exec obs --startstreaming --profile 'PiProfile' --collection 'Default'
+  exec obs --startstreaming --profile 'CdaprodOBS' --collection 'Laptop OBS Scenes'
 else
-  echo "Starting OBS in CLI mode..."
-  exec obs --startstreaming --minimize-to-tray
+  echo "Starting OBS in headless CLI mode..."
+  exec obs --startstreaming --minimize-to-tray --profile 'CdaprodOBS' --collection 'Laptop OBS Scenes'
 fi
