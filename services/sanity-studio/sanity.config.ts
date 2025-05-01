eccce9c4bebd527b14e11bd3d3b4953597f82267
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2025-05-01 14:00:37 -0400

    Add new SDK path and face camera HTML file
    - Updated .dockerignore and .gitignore to include /src/sdks
    - Added show-face-camera.html for face camera service

--- services/sanity-studio/sanity.config.ts
+++ services/sanity-studio/sanity.config.ts
@@ -1,17 +1,35 @@
-// /services/sanity-studio/sanity.config.ts
-
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { schema } from './schemas/schema'
+// @ts-ignore
+import S3AssetSource from 'sanity-plugin-asset-source-s3'
+
+const withS3 = process.env.MINIO_ENDPOINT !== undefined
 
 export default defineConfig({
   name: 'videoRenderStudio',
   title: 'Video Render Studio',
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your_project_id_here',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
-  plugins: [deskTool()],
+  plugins: [
+    deskTool(),
+    ...(withS3
+      ? [S3AssetSource({
+          clientConfig: {
+            endpoint: process.env.MINIO_ENDPOINT,
+            region: process.env.MINIO_REGION,
+            credentials: {
+              accessKeyId: process.env.MINIO_ACCESS_KEY!,
+              secretAccessKey: process.env.MINIO_SECRET_KEY!,
+            },
+            forcePathStyle: true,
+          },
+          bucket: process.env.MINIO_BUCKET!,
+        })]
+      : []),
+  ],
 
   schema: schema,
 })
